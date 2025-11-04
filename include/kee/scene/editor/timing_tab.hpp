ee220483fcf83a2d94c5d22677085a3a1138cf2f
sumandas27 <sumanta.das2727@gmail.com>
2025-11-03 22:51:57 -0500

    began work on timing ui

--- include/kee/scene/editor/timing_tab.hpp
+++ include/kee/scene/editor/timing_tab.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "kee/ui/base.hpp"
+#include "kee/ui/rect.hpp"
 #include "kee/ui/text.hpp"
+#include "kee/ui/textbox.hpp"
 
 namespace kee {
 namespace scene {
@@ -13,7 +15,18 @@ public:
     timing_tab(const kee::ui::required& reqs);
 
 private:
-    kee::ui::handle<kee::ui::text> wip_text;
+    kee::ui::handle<kee::ui::base> points_frame;
+    kee::ui::handle<kee::ui::rect> points_bg;
+
+    std::vector<kee::ui::handle<kee::ui::rect>> timing_rects;
+    kee::ui::handle<kee::ui::rect> timing_slider;
+
+    kee::ui::handle<kee::ui::base> bpm_offset_frame;
+    kee::ui::handle<kee::ui::text> bpm_text;
+    kee::ui::handle<kee::ui::textbox> bpm_textbox;
+
+    kee::ui::handle<kee::ui::text> offset_text;
+    kee::ui::handle<kee::ui::textbox> offset_textbox;
 };
 
 } // namespace editor
--- src/scene/editor/timing_tab.cpp
+++ src/scene/editor/timing_tab.cpp
@@ -14,14 +14,90 @@ timing_tab::timing_tab(const kee::ui::required& reqs) :
         ),
         false
     ),
-    wip_text(add_child<kee::ui::text>(std::nullopt,
+    points_frame(add_child<kee::ui::base>(std::nullopt,
+        pos(pos::type::rel, 0),
+        pos(pos::type::rel, 0),
+        dims(
+            dim(dim::type::rel, 0.5f),
+            dim(dim::type::rel, 1)
+        ),
+        false
+    )),
+    points_bg(points_frame.ref.add_child<kee::ui::rect>(std::nullopt,
+        raylib::Color(20, 20, 20),
+        pos(pos::type::rel, 0.5f),
+        pos(pos::type::rel, 0.5f),
+        border(border::type::rel_w, 0.05f),
+        true, std::nullopt,
+        ui::rect_roundness(ui::rect_roundness::type::rel_w, 0.05f, std::nullopt)
+    )),
+    timing_slider(add_child<kee::ui::rect>(std::nullopt,
+        raylib::Color(40, 40, 40),
+        pos(pos::type::rel, 0.55f),
+        pos(pos::type::rel, 0.4f),
+        dims(
+            dim(dim::type::rel, 0.4f),
+            dim(dim::type::rel, 0.05f)
+        ),
+        false, std::nullopt,
+        ui::rect_roundness(ui::rect_roundness::type::rel_h, 0.5f, std::nullopt)
+    )),
+    bpm_offset_frame(add_child<kee::ui::base>(std::nullopt,
+        pos(pos::type::rel, 0.725f),
+        pos(pos::type::rel, 0.75f),
+        dims(
+            dim(dim::type::rel, 0.15f),
+            dim(dim::type::rel, 0.1f)
+        ),
+        true
+    )),
+    bpm_text(bpm_offset_frame.ref.add_child<kee::ui::text>(std::nullopt,
         raylib::Color::White(),
+        pos(pos::type::rel, 0),
+        pos(pos::type::rel, 0),
+        ui::text_size(ui::text_size::type::rel_h, 0.4f),
+        false, assets.font_semi_bold, "BPM", false
+    )),
+    bpm_textbox(bpm_offset_frame.ref.add_child<kee::ui::textbox>(std::nullopt,
         pos(pos::type::rel, 0.5f),
+        pos(pos::type::rel, 0),
+        dims(
+            dim(dim::type::rel, 0.5f),
+            dim(dim::type::rel, 0.4f)
+        ),
+        false, *this
+    )),
+    offset_text(bpm_offset_frame.ref.add_child<kee::ui::text>(std::nullopt,
+        raylib::Color::White(),
+        pos(pos::type::rel, 0),
+        pos(pos::type::rel, 0.6f),
+        ui::text_size(ui::text_size::type::rel_h, 0.4f),
+        false, assets.font_semi_bold, "Offset", false
+    )),
+    offset_textbox(bpm_offset_frame.ref.add_child<kee::ui::textbox>(std::nullopt,
         pos(pos::type::rel, 0.5f),
-        ui::text_size(ui::text_size::type::rel_h, 0.07f),
-        true, assets.font_semi_bold, "WORK IN PROGRESS", false
+        pos(pos::type::rel, 0.6f),
+        dims(
+            dim(dim::type::rel, 0.5f),
+            dim(dim::type::rel, 0.4f)
+        ),
+        false, *this
     ))
-{ }
+{ 
+    timing_rects.reserve(4);
+    for (std::size_t i = 0; i < 4; i++)
+        timing_rects.emplace_back(add_child<kee::ui::rect>(std::nullopt,
+            raylib::Color(40, 40, 40),
+            pos(pos::type::rel, 0.55f + 0.095f * i),
+            pos(pos::type::rel, 0.2f),
+            dims(
+                dim(dim::type::rel, 0.065f),
+                dim(dim::type::aspect, 1)
+            ),
+            false, std::nullopt,
+            ui::rect_roundness(ui::rect_roundness::type::rel_w, 0.05f, std::nullopt)
+        ));
+}
 
 } // namespace editor
 } // namespace scene
